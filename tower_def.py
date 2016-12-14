0a24402d274d3bd7cf78bca83dbb930cedb70c68
addmao <put_1996@live.com>
2016-12-14 15:44:36 +0700

    add monster level 1

--- tower_def.py
+++ tower_def.py
@@ -8,12 +8,28 @@ class TowerDefense(arcade.Window):
 		super().__init__(width, height, title="Tower Defense")
 
 		arcade.set_background_color(arcade.color.CYAN)
+		
 		self.text_angle = 0
 		self.time_elapsed = 0
+	
+	##	self.xPosition = xPosition	
+    	##	self.yPosition = yPosition
 
+	##	self.deltaX = 0
+	##	self.deltaY = 0
+
+		self.easyMonster1 = arcade.Sprite("assets/Monster_easy1.png")
+		self.easyMonster1.set_position(100, 200)
+		
 	def animate(self, delta_time):
 		self.text_angle += 1
 		self.time_elapsed += delta_time
+
+	def canMove(self):
+		self.xPos += deltaX
+		self.yPos += deltaY
+
+		
 
 	def on_draw(self):
 		arcade.start_render()
@@ -26,6 +42,8 @@ class TowerDefense(arcade.Window):
 		start_x = 400
 		start_y = 200
 		arcade.draw_text("Time: {:5.1f}".format(self.time_elapsed), start_x, start_y, arcade.color.BLACK, 30)
+
+		self.easyMonster1.draw()
 
 if __name__ == '__main__':
 	window = TowerDefense(SCREEN_WIDTH, SCREEN_HEIGHT)	
