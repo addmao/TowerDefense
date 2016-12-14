dcb586169b3c2ad6710f786d228e7a09b05e5c6e
addmao <put_1996@live.com>
2016-12-14 15:17:43 +0700

    add timer

--- tower_def.py
+++ tower_def.py
@@ -5,15 +5,30 @@ SCREEN_HEIGHT = 600
 
 class TowerDefense(arcade.Window):
 	def __init__(self, width, height):
-		super().__init__(width, height)
+		super().__init__(width, height, title="Tower Defense")
 
-		arcade.set_background_color(arcade.color.BROWN)
+		arcade.set_background_color(arcade.color.CYAN)
+		self.text_angle = 0
+		self.time_elapsed = 0
+
+	def animate(self, delta_time):
+		self.text_angle += 1
+		self.time_elapsed += delta_time
 
 	def on_draw(self):
 		arcade.start_render()
 
+		start_x = 100
+		start_y = 100
+		arcade.draw_point(start_x, start_y, arcade.color.BLACK, 5)
+		arcade.draw_text("TOWER DEFENSE!!!", start_x, start_y, arcade.color.BLACK, 14, width=100, align="center", anchor_x="center", anchor_y="center", rotation=0.0)
+		
+		start_x = 400
+		start_y = 200
+		arcade.draw_text("Time: {:5.1f}".format(self.time_elapsed), start_x, start_y, arcade.color.BLACK, 30)
+
 if __name__ == '__main__':
-	window = TowerDefense(SCREEN_WIDTH, SCREEN_HEIGHT)
+	window = TowerDefense(SCREEN_WIDTH, SCREEN_HEIGHT)	
 	arcade.run()
 
 
