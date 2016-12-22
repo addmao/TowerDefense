50736a5dac8444c3748a38c2a90ec8be2a6ab588
addmao <put_1996@live.com>
2016-12-23 01:26:59 +0700

    several bug game --> bomb T T

--- models.py
+++ models.py
@@ -2,19 +2,31 @@ import arcade
 
 class Model:
 	def __init__(self, world, x, y, angle):
+		self.world = world		
 		self.x = x
 		self.y = y
 		self.angle = 0
-
-
-
-class EasyMonster(Model):
-	def __init__(self, x, y):
-		self.sprite_list = arcade.SpriteList()
 	
+	def hit(self, other, hit_size):
+		return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
+
+
+
+class Monster(Model):
+	def __init__(self, x, y):
+		super().__init__(world, x, y, 0)
+		##self.monster_list = arcade.SpriteList()
+		
 		self.x = x
 		self.y = y 
+		for i in range(100):
+			monster = arcade.Sprite("assets/Monster_easy1.png")
+			monster.center_x = x
+			monster.center_y = y
 
+			self.all_sprites_list.append(monster)
+			self.monster_list.append(monster)
+	
 	def animate(self, delta):
 		##easyMonster = self.easyMonster
 		##self.x += 5
@@ -45,5 +57,88 @@ class EasyMonster(Model):
 
 	def aliveMonster(self, x, y):
 		self.aliveImage = arcade.Sprite("assets/clearMonster.png")
+
+class Tower(Model):
+	def __init__(self, world, x, y):
+		super().__init__(world, x, y, 0)
+
+	def buy():
+		self.money -= 100
+
+
+class Bullet(Model):
+	def __init__(self, world, x, y):
+		super().__init__(world, x, y, 0)
+	def update(self):
+		self.center_x += 5
+		self.center_y += 5
+
+class World:
+	NUMBER_OF_MONSTER = 100
+	NUMBER_OF_BULLET = 50
+		
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+		##self.monster = Monster(self, 50, 175)
+		self.tower = Tower(self, 500, 500)
+		self.monsters = []
+		self.bullet_list = arcade.SpriteList()
+		
+		for i in range(NUMBER_OF_BULLET):
+			bullet = Bullet(self, 0, 0, 0, 0)
+			self.bullet_list.append(bullet)
+
+		for i in range(NUMBER_OF_MONSTER):
+			monster = Monster(self, 0, 0, 0, 0)
+			self.monsters.append(monster)
+
+		self.money = 100
+
+	def animate(delta):
+		self.text_angle += 1
+		self.time_elapsed += delta
+		
+		for bullet in self.bullet_list:
+			bullet.animate(delta)
+			if self.bullet.hit(monster, 10):
+				self.monster.aliveMonster()
+				self.money += 20
+	
+		for monster in self.monsters:
+			monster.animate(delta)
+			
+	def on_mouse_motion(self, x, y, button, modifiers):
+		self.tower_sprite.center_x = x
+		self.tower_sprite.center_y = y
+
+	def on_mouse_press(self, x, y, button, modifiers):
+		print(button)
+		if money == 100 and arcade.MOUSE_BUTTON_LEFT:
+			self.tower.buy()
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- tower_def.py
+++ tower_def.py
@@ -1,19 +1,29 @@
 import arcade
-from models import EasyMonster
-from tower import Tower
-from tower import Bullet
+
+from models import World, Tower, Bullet
 
 SCREEN_WIDTH = 750
 SCREEN_HEIGHT = 550
+
+class Model(arcade.Sprite):
+	def __init__(self, *args, **kwargs):
+		self.model = kwargs.pop('model', None)
+		super().__init__(*args, **kwargs)
+
+	def sync_with_model(self):
+		if self.model:
+			self.set_position(self.model.x, self.model.y)
+			self.angle = self.model.angle
+
+	def draw(self):
+		self.sync_with_model()
+		super().draw()
 
 class TowerDefense(arcade.Window):
 	def __init__(self, width, height):
 		super().__init__(width, height, title="Tower Defense")
 		
-		self.all_sprites_list = arcade.SpriteList()
-		self.monster_list = arcade.SpriteList()
 		self.bullet_list = arcade.SpriteList()		
-		self.tower_list = arcade.SpriteList()
 
 		arcade.set_background_color(arcade.color.WHITE)
 		
@@ -21,24 +31,34 @@ class TowerDefense(arcade.Window):
 		self.time_elapsed = 0
 		self.money = 0
 
-		##self.tower = Tower(300, 400)
-		self.tower_sprite = arcade.Sprite("assets/Tower1.png")
-		self.tower_sprite.center_x = 500
-		self.tower_sprite.center_y = 450	
-		self.all_sprites_list.append(self.tower_sprite)		
+		self.world = World(width, height)
+
+		self.tower_sprite = Model("assets/Tower1.png", model=self.world.tower)
+		self.monster_sprite = []
+		for monster in self.world.monsters:
+			self.monster_sprites.append(ModelSprite("assets/Monster_easy1.png", scale=0.5, model=monster))
+	
+		self.bullet_sprite = []
+		for bullet in self.world.bullets:
+			self.monster_sprites.append(ModelSprite("assets/bulletOne.png", scale=0.5, model=bullet))
+
+		for i in range(100):
+			self.monster = arcade.Sprite("assets/Monster_easy1.png")
+			self.monster.center_x = 50
+			self.monster.center_y = 175
+			self.all_sprites_list.append(self.monster)
+			self.monster_list.append(self.monster)
 
 
-		##self.bullet_list.append(self.tower_sprite)
-		##self.easyMonster = EasyMonster(50, 175)
-		self.monster = arcade.Sprite("assets/Monster_easy1.png")
-		self.monster.center_x = 50
-		self.monster.center_y = 175
-		self.all_sprites_list.append(self.monster)
+		
+		bullet = Bullet("assets/bulletOne.png", 0.5 * 1.5)
 
-		self.monster_list.append(self.monster)
-		self.bullet_list.append(self.tower_sprite)
+		bullet.center_x = self.tower_sprite.center_x
+		bullet.center_y = self.tower_sprite.center_y
 
-		##self.monster_list.append()
+		self.all_sprites_list.append(bullet)
+		self.bullet_list.append(bullet)
+		
 		##self.tower = Tower(300, 400)
 		##self.tower_sprite = arcade.Sprite("assets/Tower1.png")
 
@@ -46,72 +66,81 @@ class TowerDefense(arcade.Window):
 		##self.bulletOne_sprite = arcade.Sprite("assets/bulletOne.png")
 		
 		self.gameBoard = arcade.Sprite("assets/gameBoard.png")
-		self.gameBoard.set_position(350, 300)	
+		self.gameBoard.set_position(350, 300)
 	
 	def animate(self, delta):
-		self.text_angle += 1
-		self.time_elapsed += delta
+		self.world.animate(delta)
+		
 	
 		self.all_sprites_list.update()	
-		##bullet = self.bullet
-		##monster = self.easyMonster
-		##tower = self.tower
 		
-			
+		for bullet in self.bullet_list:
+			hit_list = arcade.check_for_collision_with_list(bullet, self.monster_list)		
+		if len(hit_list) > 0:
+			bullet.kill()
+
+		for monster in self.monster_list:
+			monster.kill()
+			self.money += 20
 		
-	
-		
-		self.money += 10
+##		if bullet.bottom > SCREEN_HEIGHT:
+##			bullet.kill()
 
 	def on_draw(self):
 		arcade.start_render()
 
 		self.all_sprites_list.draw()
-		##tower = self.tower
 		self.gameBoard.draw()
 		
+		self.tower_sprite.draw()
+		self.monster_sprite.draw()
+		self.bullet.draw()
+		for sprite in  self.monster_sprites:
+			sprite.draw()
+
 		arcade.draw_text("Time: {:5.1f}".format(self.time_elapsed), 500, 30, arcade.color.BLACK, 20)
 		arcade.draw_text("MONEY: {:5d}".format(self.money), 100, 75, arcade.color.YELLOW)
 		
-		##self.easyMonster_sprite.draw()
-		
-
 		##if self.time_elapsed % 5 == 0:
 			##self.sprite_list.append(self.easyMonster)
-		
 
-		##self.bulletOne_sprite.draw()
-		##self.bulletOne_sprite.set_position(Bullet.x, Bullet.y)	
-
-		self.tower_sprite.draw() 
-		self.tower_sprite.set_position(tower.x, tower.y)
 		self.money += 10
-
-	def on_mouse_motion(self, x, y, button, modifiers):
-		self.tower_sprite.center_x = x
-		self.tower_sprite.center_y = y
 	
-	def on_mouse_press(self, x, y, button, modifiers):
-		print(button)
-		self.tower_sprite = arcade.Sprite("assets/Tower1.png")
-		self.tower_sprite.center_x = 500
-		self.tower_sprite.center_y = 450
-		if money == 100:
-			self.all_sprites_list.append(self.tower_sprite)
-			self.tower_list.append(self.tower_sprite)	
-		##bullet = Bullet("assets/bulletOne.png", 0.5 * 1.5)
-	##	bullet.angle = 90
+				
+##class Tower:
 
-		##bullet.center_x = self.tower_sprite.center_x
-		##bullet.bottom = self.tower_sprite.top
 
-		##self.all_sprites_list.append(bullet)
-		##self.bullet_list.append(bullet)
 
-	##def on_mouse_release():
-	##	self.all_sprites_list.pop(self.tower_sprite)
-	##def on_key_press(self, key, key_modifiers):
-	##	self.towerReady.on_key_press(key, key_modifiers)		
+
+class Monster:
+	def animate(delta):
+		if self.x <= 800:
+			self.x += 5
+			print(self.x, self.y)
+		
+		if self.x >= 175 and self.y < 265:
+			self.y += 5
+			self.x = 175
+			print(self.x, self.y)
+		
+		if self.x > 265 and self.y != 365:
+			self.y += 5
+			self.x = 270
+			print(self.x, self.y)
+
+		if self.x <= 270 and self.x > 120 and self.y == 365:
+			self.x -= 10
+			print(self.x, self.y)
+
+		if self.x > 120 and self.y > 365 and self.y <= 450 :
+			self.y += 5
+			self.x = 120
+
+class Bullet(arcade.Sprite):
+        def update(self):
+                self.center_x += 5
+                self.center_y += 5
+
 
 if __name__ == '__main__':
 		window = TowerDefense(SCREEN_WIDTH, SCREEN_HEIGHT)	
